d1dfa6c490caf4c2a25eb508665e351542698bec
delokoseni <107359196+delokoseni@users.noreply.github.com>
2025-02-20 19:10:22 +0700

    First commit.

---
+++ src/main/java/com/example/Altaska/AltaskaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Altaska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltaskaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltaskaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Altaska/AltaskaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Altaska;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltaskaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
