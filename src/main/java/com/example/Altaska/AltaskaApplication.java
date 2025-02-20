8364187cc04db752a8ad348e4fa224e5e40aa2ba
delokoseni <107359196+delokoseni@users.noreply.github.com>
2025-02-20 20:35:50 +0700

    Программа запускается, настроено подключение к базе данных.

--- src/main/java/com/example/Altaska/AltaskaApplication.java
+++ src/main/java/com/example/Altaska/AltaskaApplication.java
@@ -1,5 +1,6 @@
 package com.example.Altaska;
 
+import com.example.Altaska.config.EnvLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AltaskaApplication {
 
 	public static void main(String[] args) {
+		EnvLoader.load();
 		SpringApplication.run(AltaskaApplication.class, args);
 	}
 
