a0e3620ba68500b679153dc9285a81eee39949b4
delokoseni <107359196+delokoseni@users.noreply.github.com>
2025-04-09 11:33:59 +0700

    Реализован минимальный вариант авторизации.

--- src/main/java/com/example/Altaska/controller/LoginController.java
+++ src/main/java/com/example/Altaska/controller/LoginController.java
@@ -1,11 +1,18 @@
 package com.example.Altaska.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     public  LoginController() { }
+
     @GetMapping({"/login"})
     public String ShowLogin() { return "login"; }
 }
