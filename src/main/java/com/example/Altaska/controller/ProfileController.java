fd1962f7aba353a78211ae390215d5060d24c107
delokoseni <107359196+delokoseni@users.noreply.github.com>
2025-04-30 14:56:27 +0700

    Мелкие правки.

---
+++ src/main/java/com/example/Altaska/controller/ProfileController.java
@@ -0,0 +1,17 @@
+package com.example.Altaska.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.security.Principal;
+
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile")
+    public String ShowProfile(Model model, Principal principal) {
+
+        return "profile";
+    }
+}
