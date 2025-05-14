7d98e191f53691c8a6c1bb961a24eaecb96bebad
delokoseni <107359196+delokoseni@users.noreply.github.com>
2025-05-14 12:08:51 +0700

    Перед созданием новой ветки.

--- src/main/java/com/example/Altaska/controller/SubTaskApiController.java
+++ src/main/java/com/example/Altaska/controller/SubTaskApiController.java
@@ -1,7 +1,5 @@
 package com.example.Altaska.controller;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +17,6 @@ import com.example.Altaska.models.Users;
 
 import java.security.Principal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
